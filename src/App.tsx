79bf9a93063590221412eda7a117e2765d84cd9f
alexeikutyrev <alexei.kutyrev@gmail.com>
2021-05-31 08:31:42 +0300

    pre final

--- src/App.tsx
+++ src/App.tsx
@@ -69,189 +69,104 @@ const MAX_SCALE = 24 * 12; // макс зум 5 мин; 12 - кол - во 5 м�
 const MIN_SCALE = 1.00
 
 function App() {
-  const svgRef = useRef<SVGSVGElement | null>(null)
-
-  const timeLineRef = useRef<HTMLDivElement>(null)
-  const [slicingScaleX, setSlicingScaleX] = useState<number>(1.00)
-  const [timePosition, setTimePosition] = useState<DateAPIString>('24.04.2021 23:37:02.000')
-  const [wheeling, setWheeling] = useState<any>(0)
-
-  const [timelineWidth, setTimelineWidth] = useState<number>(0)
-  const [data, setData] = useState({
-    scaleX: 1,
-    positionX: 0,
-    zoomTargetX: 0,
-    zoomPointX: 0
-  })
-
-  // const [data, setData] = useState({
-  //   scaleX: 1,
-  //   w: 0,
-  //   zoomTargetX: 0,
-  //   zoomPointX: 0
-  // })
-
-
-  const fullDurationVideoSec = fullDurationVideo / 1000
-
-  useEffect(() => {
-    setInterval(() => {
-      console.log(moment().format(API_DATE_FORMAT))
-      setTimePosition(moment().format(API_DATE_FORMAT))
-    }, 1000)
-
-    if(timeLineRef && timeLineRef.current) {
-      setTimelineWidth(timeLineRef.current.offsetWidth)
-    }
-
-    if(!svgRef || svgRef.current) return
-
-  }, [])
-
-  //handleWheelEvent в useCallback
-  const handleWheelEvent = (ev: React.WheelEvent): void => {
-    if(!timeLineRef || !timeLineRef.current) return
-    if(!svgRef || !svgRef.current) return
-
-    const { offsetLeft, offsetWidth } = timeLineRef.current
-
-    const zoomPointX = Math.round(fullDurationVideoSec * ((ev.pageX - offsetLeft) / offsetWidth * 100) / 100)
-
-    ev.preventDefault()
-
-    const delta = Math.max(-1, Math.min(1, ev.deltaY))
-
-    const zoomTargetX = (zoomPointX - data.positionX) / data.scaleX
-
-    let scaleX = data.scaleX + (delta * zoomIntensity * data.scaleX)
-    // Math.min(MAX_SCALE, Math.max(MIN_SCALE, scale - ev.deltaY / 100))
-
-    scaleX = Math.max(1, Math.min(MAX_SCALE, scaleX))
-
-    let positionX = -zoomTargetX * scaleX + zoomPointX
-
-    if(positionX > 0) positionX = 0
-
-    if(positionX + fullDurationVideoSec * scaleX < fullDurationVideoSec) positionX = -fullDurationVideoSec * (scaleX - 1)
-
-    setData({
-      scaleX: parseFloat(scaleX.toFixed(3)),
-      zoomPointX: zoomPointX,
-      zoomTargetX: zoomTargetX,
-      positionX: parseFloat(positionX.toFixed(2)),
-    })
-  }
-
-  const xStartVisibleSegment = Math.round(Math.abs(data.positionX) / data.scaleX)
-  const xEndVisibleSegment = Math.round(xStartVisibleSegment + (fullDurationVideoSec / data.scaleX))
-
-  const startVisibleTime = moment(moment(startTime, API_DATE_FORMAT).valueOf() + (xStartVisibleSegment * 1000)).format(API_DATE_FORMAT)
-  const endVisibleTime = moment(moment(startTime, API_DATE_FORMAT).valueOf() + (xEndVisibleSegment * 1000)).format(API_DATE_FORMAT)
-
   return (
     <div className="App">
-      <div>xStartVisibleSegment {xStartVisibleSegment} sec</div>
-      <div>xEndVisibleSegment {xEndVisibleSegment} sec</div>
-      <div>Продолжительность сегмента {xEndVisibleSegment - xStartVisibleSegment} sec</div>
-      <div>startVisibleTime {startVisibleTime}</div>
-      <div>endVisibleTime {endVisibleTime}</div>
-      <div>{startTime}</div>
-      <div>{endTime}</div>
-      <pre>
-        {JSON.stringify(contextSegments, null, 2)}
-      </pre>
 
-      <div ref={timeLineRef} onWheel={handleWheelEvent}>
-        <svg width={`${timelineWidth}`}
-             ref={svgRef}
-             height="26"
-             viewBox={`0 0 ${fullDurationVideoSec} 26`}
-             preserveAspectRatio="none"
-             fill="#C2C2C2"
-             xmlns="http://www.w3.org/2000/svg">
-          <rect width={`${fullDurationVideoSec}`} x={0} height="26" fill="#34404A"/>
-          <rect width={`${fullDurationVideoSec}`} x={0} height="16" fill="#C2C2C2"/>
-          <g className={'slider'}>
-            <rect
-                width={`300`}
-                height="16"
-                x={0}
-                fill="#EB5757"/>
-              {/*<text*/}
-              {/*    y={20}*/}
-              {/*    width={500}*/}
-              {/*    x={segmentDuration(timePosition, startTime) / 1000} style={{*/}
-              {/*      fontSize: 12*/}
-              {/*}}>{timePosition}</text>*/}
-          </g>
-          <g className={'contextSegments'}
-             transform={`translate(${data.positionX}) scale(${data.scaleX}, 1)`}>
-            {contextSegments.map(item => {
-              const width = segmentDuration(item.end, item.start) / 1000
-              const x = segmentDuration(item.start, startTime) / 1000
+      <SimpleCanvasExample />
 
-              return <rect
-                  xlinkTitle={`${item.start} - ${item.end}`}
-                  opacity="0.7"
-                  y="0"
-                  height="8"
-                  fill="#0098BA"
-                  x={x}
-                  width={width}
-              />
-            })}
-          </g>
-          {/*transform={`translate(${data.positionX}) scale(${data.scaleX}, 1)`}*/}
-          <g className={'videoSegments'}
-             transform={`translate(${data.positionX}) scale(${data.scaleX}, 1)`}>
-            {videoSegments.map(item => {
-              const width = segmentDuration(item.end, item.start) / 1000
-              const x = segmentDuration(item.start, startTime) / 1000
+      {/*<div ref={timeLineRef} onWheel={handleWheelEvent} onClick={(ev) => {*/}
+      {/*  if(!timeLineRef || !timeLineRef.current) return*/}
 
-              return <rect
-                  opacity="0.7"
-                  y="0"
-                  height="16"
-                  fill="#768089"
-                  x={x}
-                  width={width}
-              />
-            })}
-          </g>
-          <g clipPath="url(#clip0)">
-            <line x1="28.5" y1="18" x2="28.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="250.5" y1="18" x2="250.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="475.5" y1="18" x2="475.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="139.5" y1="18" x2="139.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="364.5" y1="18" x2="364.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="586.5" y1="18" x2="586.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="642.5" y1="18" x2="642.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="56.5" y1="18" x2="56.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="278.5" y1="18" x2="278.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="503.5" y1="18" x2="503.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="167.5" y1="18" x2="167.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="392.5" y1="18" x2="392.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="614.5" y1="18" x2="614.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="0.5" y1="18" x2="0.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="223.5" y1="18" x2="223.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="447.5" y1="18" x2="447.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="111.5" y1="18" x2="111.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="336.5" y1="18" x2="336.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="559.5" y1="18" x2="559.5" y2="23" stroke="#7E7E7E"/>
-            <line x1="83.5" y1="18" x2="83.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="306.5" y1="18" x2="306.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="531.5" y1="18" x2="531.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="195.5" y1="18" x2="195.5" y2="28" stroke="#7E7E7E"/>
-            <line x1="419.5" y1="18" x2="419.5" y2="28" stroke="#7E7E7E"/>
-          </g>
-          <defs>
-            <clipPath id="clip0">
-              <rect width={`${timelineWidth}`} height="10" fill="white" transform="translate(0 18)"/>
-            </clipPath>
-          </defs>
-        </svg>
+      {/*  const { offsetLeft, offsetWidth } = timeLineRef.current*/}
 
-      </div>
+      {/*  const zoomPointX = Math.round((fullDurationVideoSec * ((ev.pageX - offsetLeft) / offsetWidth * 100) / 100) / data.scaleX)*/}
+
+      {/*  console.log(moment(moment(startTime, API_DATE_FORMAT).valueOf() + (xStartVisibleSegment * 1000) + (zoomPointX * 1000)).format(API_DATE_FORMAT))*/}
+      {/*}}>*/}
+      {/*  <svg width={`${timelineWidth}`}*/}
+      {/*       ref={svgRef}*/}
+      {/*       height="26"*/}
+      {/*       viewBox={`0 0 ${fullDurationVideoSec} 26`}*/}
+      {/*       preserveAspectRatio="none"*/}
+      {/*       fill="#C2C2C2"*/}
+      {/*       xmlns="http://www.w3.org/2000/svg">*/}
+      {/*    <rect width={`${fullDurationVideoSec}`} x={0} height="26" fill="#34404A"/>*/}
+      {/*    <rect width={`${fullDurationVideoSec}`} x={0} height="16" fill="#C2C2C2"/>*/}
+      {/*    <g className={'slider'}>*/}
+      {/*      <rect*/}
+      {/*          width={`300`}*/}
+      {/*          height="16"*/}
+      {/*          x={0}*/}
+      {/*          fill="#EB5757"/>*/}
+      {/*    </g>*/}
+      {/*    <g className={'contextSegments'}*/}
+      {/*       transform={`translate(${data.positionX}) scale(${data.scaleX}, 1)`}>*/}
+      {/*      {contextSegments.map(item => {*/}
+      {/*        const width = segmentDuration(item.end, item.start) / 1000*/}
+      {/*        const x = segmentDuration(item.start, startTime) / 1000*/}
+
+      {/*        return <rect*/}
+      {/*            xlinkTitle={`${item.start} - ${item.end}`}*/}
+      {/*            opacity="0.7"*/}
+      {/*            y="0"*/}
+      {/*            height="8"*/}
+      {/*            fill="#0098BA"*/}
+      {/*            x={x}*/}
+      {/*            width={width}*/}
+      {/*        />*/}
+      {/*      })}*/}
+      {/*    </g>*/}
+      {/*    /!*transform={`translate(${data.positionX}) scale(${data.scaleX}, 1)`}*!/*/}
+      {/*    <g className={'videoSegments'}*/}
+      {/*       transform={`translate(${data.positionX}) scale(${data.scaleX}, 1)`}>*/}
+      {/*      {videoSegments.map(item => {*/}
+      {/*        const width = segmentDuration(item.end, item.start) / 1000*/}
+      {/*        const x = segmentDuration(item.start, startTime) / 1000*/}
+
+      {/*        return <rect*/}
+      {/*            opacity="0.7"*/}
+      {/*            y="0"*/}
+      {/*            height="16"*/}
+      {/*            fill="#768089"*/}
+      {/*            x={x}*/}
+      {/*            width={width}*/}
+      {/*        />*/}
+      {/*      })}*/}
+      {/*    </g>*/}
+      {/*    <g clipPath="url(#clip0)">*/}
+      {/*      <line x1="28.5" y1="18" x2="28.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="250.5" y1="18" x2="250.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="475.5" y1="18" x2="475.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="139.5" y1="18" x2="139.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="364.5" y1="18" x2="364.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="586.5" y1="18" x2="586.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="642.5" y1="18" x2="642.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="56.5" y1="18" x2="56.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="278.5" y1="18" x2="278.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="503.5" y1="18" x2="503.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="167.5" y1="18" x2="167.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="392.5" y1="18" x2="392.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="614.5" y1="18" x2="614.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="0.5" y1="18" x2="0.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="223.5" y1="18" x2="223.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="447.5" y1="18" x2="447.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="111.5" y1="18" x2="111.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="336.5" y1="18" x2="336.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="559.5" y1="18" x2="559.5" y2="23" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="83.5" y1="18" x2="83.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="306.5" y1="18" x2="306.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="531.5" y1="18" x2="531.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="195.5" y1="18" x2="195.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*      <line x1="419.5" y1="18" x2="419.5" y2="28" stroke="#7E7E7E"/>*/}
+      {/*    </g>*/}
+      {/*    <defs>*/}
+      {/*      <clipPath id="clip0">*/}
+      {/*        <rect width={`${timelineWidth}`} height="10" fill="white" transform="translate(0 18)"/>*/}
+      {/*      </clipPath>*/}
+      {/*    </defs>*/}
+      {/*  </svg>*/}
+
+      {/*</div>*/}
     </div>
   );
 }
--- src/canva.tsx
+++ src/canva.tsx
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import moment from 'moment'
+import { Layer, Stage, Rect, Text } from 'react-konva'
+import Konva from 'konva'
 
 const startDay = '20.04.2021'
 const endDay = '21.04.2021'
@@ -55,14 +57,32 @@ const segmentDuration = (endSegment: DateAPIString, startSegment: DateAPIString)
 
 const fullDurationVideo = segmentDuration(endTime, startTime)
 
-const zoomIntensity = 0.01;
-const MAX_SCALE = 24 * 12; // макс зум 5 мин; 12 - кол - во 5 минут в часах
-const MIN_SCALE = 1.00
+const zoomIntensity = 0.1
+const MAX_SCALE = 24 * 12 // макс зум 5 мин; 12 - кол - во 5 минут в часах
+// const MIN_SCALE = 1.0
+
+// user story: При отображении таймлайна используются дискретные значения: 5, 10 и 20 минут, 2, 4, 8, 12 и 24 часа.
+// ассоциация времени в ms к scaleX
+const durationZoomLevel = {
+    300000: 288,
+    600000: 144,
+    1200000: 72,
+    7200000: 12,
+    14400000: 6,
+    28800000: 3,
+    57600000: 1.5,
+    86400000: 1,
+}
+
+let scaleBy = 1.01;
 
 
 const SimpleCanvasExample: React.FC<{}> = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const canvasRefCtx = useRef<CanvasRenderingContext2D | null>(null)
+    const segments = 24
+    const layerRef = React.useRef<Konva.Layer>(null)
+    const stageRef = React.useRef<Konva.Stage>(null)
 
     useEffect(() => {
         // Initialize
@@ -77,28 +97,18 @@ const SimpleCanvasExample: React.FC<{}> = () => {
         const width = ctx.canvas.width
         const height = ctx.canvas.height
 
-        // ctx.fillRect(25,25,100,100);
-        // ctx.fill();
-        // ctx.clearRect(45,45,60,60);
-        // ctx.strokeRect(50,50,50,50);
-        // ctx.fillRect(15,55,25,25);
-        // ctx.fillStyle = '#ff0000'
         ctx.beginPath();
-        ctx.rect(0,0, 86401001 / 1000, height); // ctx.canvas.height
-        ctx.fillStyle = "black";
+        ctx.rect(0,0, width, height); // ctx.canvas.height
+        ctx.fillStyle = "#34404A";
         ctx.fill();
 
-        ctx.fillStyle = 'blue'
-        ctx.fillRect(0, 0, 10.232, height);
-        ctx.fillStyle = 'red'
-        ctx.fillRect(112, 0, 10.232, height);
-        ctx.stroke()
-
-        setTimeout(() => {
-            ctx.translate(100, 0)
-            ctx.scale(1.6, 1)
-            ctx.translate(-100, 0)
-        })
+        for(let i = 0; i <= 24; i++) {
+            ctx.fillStyle = 'red'
+            const positionX = width / segments * i
+            const isOdd = i % 2 !== 0
+            const heightLine = isOdd ? height : height - 5
+            ctx.fillRect(positionX, 0, 2, heightLine);
+        }
 
         canvas.addEventListener('wheel', (ev) => {
             console.log(ev)
@@ -113,8 +123,68 @@ const SimpleCanvasExample: React.FC<{}> = () => {
         }, { passive: true })
     }, []);
 
+    const handleWheel = (ev: Konva.KonvaEventObject<WheelEvent>) => {
+        ev.evt.preventDefault();
+        
+        if(!layerRef || !layerRef.current) return
+        if(!stageRef || !stageRef.current) return
 
-    return <canvas ref={canvasRef} width={1920}  height={50}/>
+        console.log(layerRef.current.scaleX())
+
+        var oldScale = layerRef.current.scaleX();
+
+        var pointer = stageRef.current.getPointerPosition();
+
+        var mousePointTo = {
+            // @ts-ignore
+            x: (pointer.x - layerRef.current.x()) / oldScale,
+            // @ts-ignore
+            y: (pointer.y - layerRef.current.y()) / oldScale,
+        };
+
+        var newScale =
+            ev.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+        layerRef.current.scale({ x: newScale, y: 1 });
+
+        var newPos = {
+            // @ts-ignore
+            x: pointer.x - mousePointTo.x * newScale,
+            // @ts-ignore
+            y: pointer.y - mousePointTo.y * newScale,
+        };
+        layerRef.current.position(newPos);
+    }
+
+
+    return (
+        <Stage width={window.innerWidth} height={10} ref={stageRef}>
+            <Layer ref={layerRef} onWheel={handleWheel}>
+                <Rect
+                    x={0}
+                    y={0}
+                    width={window.innerWidth}
+                    height={10}
+                    fill={'#34404A'}
+                />
+                {new Array(24).fill('').map((item, index) => {
+                    const positionX = window.innerWidth / segments * index
+                    const isOdd = index % 2 !== 0
+                    const heightLine = isOdd ? 10 : 5
+
+                    return (
+                        <Rect
+                            x={positionX}
+                            y={0}
+                            width={1}
+                            height={heightLine}
+                            fill={'#7E7E7E'}
+                        />
+                    )
+                })}
+            </Layer>
+        </Stage>
+    )
 };
 
 export default SimpleCanvasExample;
